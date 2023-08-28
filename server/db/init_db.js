380a3e40109214031baa2a72be58c38df3888e36
Cristobal Dominguez <cristobald@gmail.com>
2023-08-28 09:10:15 +0200

    Mejora el archivo Seed

--- server/db/init_db.js
+++ server/db/init_db.js
@@ -1,4 +1,5 @@
 import getPool from './pool.js'
+import encryptPassword from '../helpers/encrypt_password.js'
 
 const init = async () => {
   let connection
@@ -7,12 +8,13 @@ const init = async () => {
     connection = await getPool()
 
     console.log('---- Iniciando modificación de db ----')
-
     console.log('Borrando tablas')
     await connection.query('DROP TABLE IF EXISTS photos')
     await connection.query('DROP TABLE IF EXISTS entries')
     await connection.query('DROP TABLE IF EXISTS users')
-    console.log('Tablas borradas \n')
+    console.log('Tablas borradas\n')
+
+    console.log('Creando nuevas tablas\n')
 
     // Users
     console.log('- Creando tabla users')
@@ -31,10 +33,10 @@ const init = async () => {
 
         createdAt DATETIME DEFAULT NOW(),
         modifiedAt DATETIME,
-
+      
         PRIMARY KEY (id)
       )
-     `)
+    `)
     console.log('- Tabla users creada\n')
 
     // Entries
@@ -74,8 +76,66 @@ const init = async () => {
     `)
     console.log('- Tabla photos creada\n')
     console.log('Tablas creadas')
+
+    console.log('Creando registros\n')
+    console.log('- Agregando usuarios')
+    const users = [
+      { username: 'juanperez', email: 'juan@example.com', password: '123123', active: 1, role: 'admin' },
+      { username: 'pedroperez', email: 'pedro@example.com', password: '123123', active: 1, role: 'DEFAULT' },
+      { username: 'diegoperez', email: 'diego@example.com', password: '123123', active: 1, role: 'DEFAULT' }
+    ]
+
+    const usersQueryList = []
+    for (const { password, username, email, role, active } of users) {
+      const pwd = await encryptPassword({ password })
+      const roleField = role === 'admin' ? '\'admin\'' : role
+
+      usersQueryList.push(`('${username}', '${email}', '${pwd}', ${roleField}, ${active})`)
+    }
+
+    await connection.query(`
+    INSERT INTO users(username, email, password, role, active)
+      VALUES${usersQueryList.join(',')};
+    `)
+
+    console.log('- Usuarios creados\n')
+
+    // Entries
+    console.log('- Agregando entries')
+    await connection.query(`
+      INSERT INTO entries(title, place, description, userId)
+      VALUES('El amour!', 'Paris', 'Un gran lugar para visitar', 3),
+            ('Vacaciones por siempre', 'Bora Bora', 'Tienes que venir!', 1),
+            ('Wow, miren la vista!', 'Parque nacional de los Glaciares', 'Es un gran lugar, pero frío', 2),
+            ('Todos los caminos llegan a Roma!', 'Roma', 'La cuna de una gran civilización', 1),
+            ('Santa Marta es puro sabor', 'Santa Marta', 'Una ciudad de diversidades; en su paisaje, en su gente y en cada aspecto cultural.', 1),
+            ('Muchas maneras de disfrutar', 'Islandia', 'Acogedoras comarcas, montañas espectaculares e islas frente a las costas hacen de Islandia un mundo único.', 1);
+    `)
+    console.log('- Entries creadas\n')
+
+    // Photos
+    console.log('- Agregando photos')
+    await connection.query(`
+    INSERT INTO photos(name, entryId)
+    VALUES('paris.jpg', 1),
+          ('bora_bora.jpg', 2),
+          ('glacier_national_park.jpg', 3),
+          ('rome01.jpg', 4),
+          ('rome02.jpg', 4),
+          ('santa_marta01.jpg', 5),
+          ('santa_marta02.jpg', 5),
+          ('santa_marta03.jpg', 5),
+          ('iceland01.jpg', 6),
+          ('iceland02.jpg', 6),
+          ('iceland03.jpg', 6);
+    `)
+    console.log('- Photos creadas\n')
+
+    console.log('Registros creados')
+
+    console.log('-------------- db Lista --------------')
   } catch (error) {
-    console.log(error)
+    console.error(error)
   } finally {
     if (connection) connection.release()
     process.exit()
