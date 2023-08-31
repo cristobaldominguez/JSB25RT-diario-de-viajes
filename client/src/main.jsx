c37ead97534de0805d32d205d054d95e9f459a05
Cristobal Dominguez <cristobald@gmail.com>
2023-08-31 18:02:18 +0200

    Agrega Tailwind

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 import App from './App.jsx'
 import AuthProvider from './providers/AuthProvider.jsx'
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
