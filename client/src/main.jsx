5de206b1deb8155d3180dba3c25685063be6c691
Cristobal Dominguez <cristobald@gmail.com>
2023-08-31 18:01:11 +0200

    Agrega React Router Dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
 import App from './App.jsx'
 import AuthProvider from './providers/AuthProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>,
 )
